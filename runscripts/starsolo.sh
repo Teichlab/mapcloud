dec0b9e645295ef309ce4fe99afe62bc8e7df0b3
ktpolanski <krzysztof_polanski@o2.pl>
2020-06-23 12:38:45 +0100

    rearrange starsolo to no longer be rudimentary

---
+++ runscripts/starsolo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+#set reference here. must be present as a star index folder in ~/STAR-2.7.3a
+REFERENCE=GRCh38
+#choose 10x chemistry. v2, v3 for 3'; or v2-5 if 5'
+CHEMISTRY=v2
+
+for SAMPLE in 
+do
+	#run the starsolo wrapper
+	bash /mnt/mapcloud/scripts/starsolo/starsolo-wrapper.sh $SAMPLE $REFERENCE $CHEMISTRY
+	
+	#dump the thing on irods and reset
+	bash /mnt/mapcloud/scripts/irods-upload.sh 10X $SAMPLE
+	rm -r $SAMPLE
+done
+
+#iRODS diagnostic - how much space is in use?
+iquest "%s" "select sum(DATA_SIZE) where COLL_NAME like '/archive/HCA/%'" | while read B dummy;do echo "CURRENT /archive/HCA USE: "$((B/(2**40)))" TiB";done
---
+++ scripts/starsolo/starsolo-wrapper.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -e
+
+#run with three positional arguments, like so:
+SAMPLE=$1
+REFERENCE=$2
+CHEMISTRY=$3
+
+#translate chemistry to actual starsolo parameters
+if [[ $CHEMISTRY == "v2" ]]
+then
+	UMI=10
+	STRAND=Forward
+elif [[ $CHEMISTRY == "v3" ]]
+then
+	UMI=12
+	STRAND=Forward
+elif [[ $CHEMISTRY == "v2-5" ]]
+then
+	UMI=10
+	STRAND=Reverse
+	#set this to v2 for barcode purposes
+	CHEMISTRY=v2
+fi
+
+#set up nested folder architecture to match /archive/HCA irods design
+mkdir $SAMPLE && cd $SAMPLE && mkdir starsolo && cd starsolo
+
+#acquire fastqs via standard 10x proceedings
+mkdir fastq && cd fastq
+printf '#!/bin/bash\nset -e\n\n----\n' > imeta.sh
+imeta qu -z seq -d sample = $SAMPLE and type = cram and target = 1 >> imeta.sh
+sed ':a;N;$!ba;s/----\ncollection:/iget -K/g' -i imeta.sh
+sed ':a;N;$!ba;s/\ndataObj: /\//g' -i imeta.sh
+bash imeta.sh && rm imeta.sh && rm -f *#888.cram
+parallel bash /mnt/mapcloud/scripts/10x/cramfastq.sh ::: *.cram
+cat *R1_001.fastq.gz > R1.fastq.gz
+cat *R2_001.fastq.gz > R2.fastq.gz
+rm *cram* && cd ..
+
+#actually run starsolo
+~/STAR-2.7.3a/bin/Linux_x86_64/STAR --runThreadN `grep -c ^processor /proc/cpuinfo` \
+	--genomeDir ~/STAR-2.7.3a/$REFERENCE \
+	--readFilesIn fastq/R2.fastq.gz fastq/R1.fastq.gz \
+	--readFilesCommand zcat \
+	--outSAMtype BAM SortedByCoordinate \
+	--outSAMmultNmax 1 \
+	--outMultimapperOrder Random \
+	--runRNGseed 1 \
+	--outSAMattributes NH HI AS nM CB UB GX GN \
+	--soloType CB_UMI_Simple \
+	--soloCBwhitelist ~/STAR-2.7.3a/whitelists/$CHEMISTRY.txt \
+	--soloBarcodeReadLength 0 \
+	--soloUMIlen $UMI \
+	--soloStrand $STRAND \
+	--soloUMIfiltering MultiGeneUMI \
+	--soloCellFilter CellRanger2.2 3000 0.99 10 \
+	--soloFeatures Gene GeneFull Velocyto \
+	--soloOutFileNames logs/ genes.tsv barcodes.tsv matrix.mtx
+
+#starsolo's take on velocyto is a three value column mtx, with spliced, unspliced, ambiguous
+#in order to let downstream tools make use of it, split them up into the appropriate matrices
+head -n 3 logs/Velocyto/raw/matrix.mtx > logs/Velocyto/raw/spliced.mtx
+head -n 3 logs/Velocyto/raw/matrix.mtx > logs/Velocyto/raw/unspliced.mtx
+head -n 3 logs/Velocyto/raw/matrix.mtx > logs/Velocyto/raw/ambiguous.mtx
+tail -n +4 logs/Velocyto/raw/matrix.mtx | cut -f 1,2,3 -d ' ' >> logs/Velocyto/raw/spliced.mtx
+tail -n +4 logs/Velocyto/raw/matrix.mtx | cut -f 1,2,4 -d ' ' >> logs/Velocyto/raw/unspliced.mtx
+tail -n +4 logs/Velocyto/raw/matrix.mtx | cut -f 1,2,5 -d ' ' >> logs/Velocyto/raw/ambiguous.mtx
+
+#do various python'y things - cellranger 3 cell filter, scrublet, velocyto object
+python3 /mnt/mapcloud/scripts/starsolo/postprocess.py
+
+#reshuffle some files and run soupX
+mv logs counts && mkdir logs && mv counts/Barcodes.stats logs && mv Log.out logs && mv Log.progress.out logs && rm -r fastq
+Rscript /mnt/mapcloud/scripts/starsolo/soupx.R
+#for whatever reason this creates this empty, unnecessary file. kick it
+rm -f Rplots.pdf
