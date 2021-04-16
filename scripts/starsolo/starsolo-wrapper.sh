155a79c5ba925929867c8fc75a0cd8e24d49e063
ktpolanski <krzysztof_polanski@o2.pl>
2021-04-16 13:04:28 +0100

    manual checksum for starsolo cram download

--- scripts/starsolo/starsolo-wrapper.sh
+++ scripts/starsolo/starsolo-wrapper.sh
@@ -30,9 +30,25 @@ mkdir $SAMPLE && cd $SAMPLE && mkdir starsolo && cd starsolo
 mkdir fastq && cd fastq
 printf '#!/bin/bash\nset -e\n\n----\n' > imeta.sh
 imeta qu -z seq -d sample = $SAMPLE and type = cram and target = 1 >> imeta.sh
-sed ':a;N;$!ba;s/----\ncollection:/iget -K/g' -i imeta.sh
+#this used to be -iget -K, but there have been some bizarre ghost errors early Q2 2021
+#yielding irreproducible borked reads at a rare rate without scripts breaking
+#so as a precaution, do a manual md5sum check instead, just in case it's a ghost irods error
+#(can skip the -K for run time as will check md5sums later)
+sed ':a;N;$!ba;s/----\ncollection:/iget/g' -i imeta.sh
 sed ':a;N;$!ba;s/\ndataObj: /\//g' -i imeta.sh
-bash imeta.sh && rm imeta.sh && rm -f *#888.cram
+bash imeta.sh
+for IRPATH in `grep "iget" imeta.sh | sed "s/iget //"`
+do
+	FID=`basename $IRPATH`
+	MD5LOCAL=`md5sum $FID| cut -f -1 -d " "`
+	MD5IRODS=`imeta ls -d $IRPATH md5 | grep 'value:' | sed 's/value: //'`
+	if [ $MD5LOCAL != $MD5IRODS ]
+	then
+		echo "md5sum conflict encountered for $FID"
+		exit 1
+	fi
+done
+rm imeta.sh && rm -f *#888.cram
 parallel bash /mnt/mapcloud/scripts/10x/cramfastq.sh ::: *.cram
 cat *R1_001.fastq.gz > R1.fastq.gz
 cat *R2_001.fastq.gz > R2.fastq.gz
