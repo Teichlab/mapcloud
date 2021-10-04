16ec90c67cc082627952e8616d891ebd8ad76d71
ktpolanski <krzysztof_polanski@o2.pl>
2021-10-04 15:16:52 +0100

    mapcloud conda yml

--- setup/conda_env_notes.sh
+++
@@ -1,7 +0,0 @@
-conda install -c conda-forge singularity
-conda install -c anaconda docopt
-conda install -c conda-forge parallel
-conda install -c bioconda samtools
-conda install -c bioconda biobambam
-conda install -c conda-forge braceexpand
-conda install -c conda-forge r-soupx
